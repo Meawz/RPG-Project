aae48cc1d5a6b8672dad901f2744130908060512
Meawz <123901325+Meawz@users.noreply.github.com>
2024-12-28 16:58:06 +0200

    Added Stats and Modifiers

---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment olditem)
+    {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if (olditem != null)
+        {
+            armor.RemoveModifier(olditem.armorModifier);
+            damage.RemoveModifier(olditem.damageModifier);
+        }
+    }
+
+}
