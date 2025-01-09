2df60fce89a5bca3ca03f03eea1ab0f6a3bef28f
Meawz <123901325+Meawz@users.noreply.github.com>
2025-01-09 20:11:50 +0200

    Level1

---
+++ Assets/Characters/Skeleton Character/Demo/Skeleton_Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skeleton_Character
+{
+	
+
+
+public class Skeleton_Rotate : MonoBehaviour
+{
+    public int rotateSpeed = 13;
+	void Update () 
+	{
+		transform.Rotate (0,rotateSpeed * Time.deltaTime,0);
+	}
+}
+
+}
